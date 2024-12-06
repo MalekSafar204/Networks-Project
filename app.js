c06b53f5664a4b9bda84d07241ed7c753f1cefe1
Malek-Safar <malek.aly@student.guc.edu.eg>
2024-12-06 23:27:43 +0200

    mongo db not working (pc)

--- app.js
+++ app.js
@@ -1,58 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-const { name } = require('ejs');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+const { name } = require("ejs");
+var { MongoClient } = require("mongodb");
 
 var app = express();
 
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.get('/', function(req, res) {
-//   res.render('index', {title: 'Networks Project', name: 'Malek'});
-// });
-
-// app.listen(3000);
-
-
-const users = [
-  { username: 'johnDoe', password: '1234' },
-  { username: 'janeSmith', password: 'password' },
-];
-
-// Route to render the login page
-app.get('/', (req, res) => {
-  res.render('index', { error: null });
+app.get("/", function (req, res) {
+  res.render("index", { title: "Networks Project", name: "Malek" });
 });
 
 // Route to handle login
-app.post('/login', (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
+});
 
-  // Check if the user exists and the password is correct
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
-
-  if (user) {
-    res.redirect('/home'); // Redirect to home page if login is successful
-  } else {
-    res.render('index', { error: 'Invalid username or password' });
-  }
+MongoClient.connect("mongodb://localhost:27017/myDB", function (err, client) {
+  client.collection("myCollection").insertOne({ name: "malek", age: 24, city: "Egypt" });
 });
 
 // Route to render the home page
-app.get('/home', (req, res) => {
-  res.send('<h1>Welcome to the Home Page!</h1>');
+app.get("/home", (req, res) => {
+  res.render("home", { title: "Home Page" });
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.listen(3000);
